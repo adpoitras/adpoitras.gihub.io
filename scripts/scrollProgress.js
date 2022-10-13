271f40a9791e0305756eaa84c948d465d9b0ec4a
Adrian <adpoitras@gmail.com>
2022-10-12 23:38:28 -0700

    reversed scroll progress bar

--- scripts/scrollProgress.js
+++ scripts/scrollProgress.js
@@ -5,10 +5,5 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  if(scrolled == 0){
-    document.getElementById("myBar").style.width = "100%";
-  }
-  else{
-    document.getElementById("myBar").style.width = scrolled + "%";
-  }
+    document.getElementById("myBar").style.width = (100-scrolled) + "%";
 }
